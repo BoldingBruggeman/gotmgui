74a18bb9777f2529c64aaae6f00170e78c97605e
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2020-10-01 18:19:34 +0100

    clean-up

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
 from setuptools import setup, find_packages
 
-try:
-    import wheel.bdist_wheel
-    class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
-        def finalize_options(self):
-            wheel.bdist_wheel.bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-        def get_tag(self):
-            python, abi, plat = wheel.bdist_wheel.bdist_wheel.get_tag(self)
-#            python, abi = 'py2.py3', 'none'
-            return python, abi, plat
-except ImportError:
-    bdist_wheel = None
-
-
 def readme():
     with open('README.md') as f:
         return f.read()
 
 setup(
       name='gotmgui',
-      version='0.1.1',
+      version='0.1.2',
       description=' --- update - ',
 #      long_description=readme(),
       url='http://github.com/BoldingBruggeman/gotmgui',
@@ -51,6 +37,5 @@ setup(
                                 '*.so', '*.dll', '*.dylib',
                                ],
                    }, 
-      cmdclass={'bdist_wheel': bdist_wheel},
 #      include_package_data=True,
       zip_safe=False)
